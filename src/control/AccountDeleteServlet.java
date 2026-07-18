f8dbf0b6a1d5f91bf53ba4569f253a5da17edd55
Salvatore <sasamocciaf@gmail.com>
2026-07-19 00:41:48 +0200

    aggiunto prefisso /common/ al path della servlet

--- src/control/AccountDeleteServlet.java
+++ src/control/AccountDeleteServlet.java
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.UtenteBean;
 
-@WebServlet("/account/delete")
+@WebServlet("/common/account/delete")
 public class AccountDeleteServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private UtenteDAO utenteDAO;
