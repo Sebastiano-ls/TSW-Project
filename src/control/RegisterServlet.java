c178712382a917a83ebfb4145a83ccebcc612dca
Salvatore <sasamocciaf@gmail.com>
2026-07-19 00:42:25 +0200

    aggiornato forward verso views/ invece di views/common/

--- src/control/RegisterServlet.java
+++ src/control/RegisterServlet.java
@@ -33,7 +33,7 @@ public class RegisterServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/views/common/register.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/views/register.jsp").forward(request, response);
     }
 
     @Override
