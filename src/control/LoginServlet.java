354a151686a8b426edfdac85b59aba8cbf94481e
Salvatore <sasamocciaf@gmail.com>
2026-07-19 00:42:04 +0200

    aggiornato forward verso views/ invece di views/common/

--- src/control/LoginServlet.java
+++ src/control/LoginServlet.java
@@ -32,7 +32,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/views/common/login.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
     }
 
     @Override
