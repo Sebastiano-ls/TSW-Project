b3b0ea2940de3827646168530e2f2d0f02c74774
Seba <seby.lo.fra@gmail.com>
2026-07-20 04:59:23 +0200

    AGGIUNTA FEAUTURE TRACCIAMENTO DELLO USER AGENT COME CONSIGLIATO IN LEZ CENNI AVANZATI + INSTANZIAZIONE DEL CARRELLO VUOTO SE LA SESSIONE L'USER è NUOVO

--- src/control/HomeServlet.java
+++ src/control/HomeServlet.java
@@ -3,9 +3,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -29,12 +31,38 @@ public class HomeServlet extends HttpServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        HttpSession session = request.getSession();
+        
+        if (session.getAttribute("userAgent") == null) {
+            String userAgent = request.getHeader("User-Agent");
+            String browser = "Sconosciuto";
+
+            if (userAgent != null) {
+                if (userAgent.contains("Edg")) {
+                    browser = "Edge";
+                } else if (userAgent.contains("OPR") || userAgent.contains("Opera")) {
+                    browser = "Opera";
+                } else if (userAgent.contains("Chrome")) {
+                    browser = "Chrome";
+                } else if (userAgent.contains("Firefox")) {
+                    browser = "Firefox";
+                } else if (userAgent.contains("Safari")) {
+                    browser = "Safari";
+                }
+            }
+            
+            session.setAttribute("userAgent", browser);
+        }
+
         try{
             List<CrocieraBean> lastMinute = crocieraDAO.doRetrieveAllAttivi();
             request.setAttribute("crociereLastMinute", lastMinute);
         }catch(SQLException e){
             throw new ServletException(e);
         }
+
+        if (session.getAttribute("carrello") == null)
+            session.setAttribute("carrello", new ArrayList<CrocieraBean>());
 
         request.getRequestDispatcher("/WEB-INF/views/common/home.jsp").forward(request, response);
     }
